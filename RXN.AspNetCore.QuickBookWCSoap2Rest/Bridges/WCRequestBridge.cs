b1bbf98b4fd2fbd297fd1e72aa6ba2b4eb4da81d
Thang.Le <thang.rxn@gmail.com>
2021-01-15 17:53:59 +0700

    Remove try/catch Fix typo in variable name Add package description to nuget

--- RXN.AspNetCore.QuickBookWCSoap2Rest/Bridges/WCRequestBridge.cs
+++ RXN.AspNetCore.QuickBookWCSoap2Rest/Bridges/WCRequestBridge.cs
@@ -4,7 +4,6 @@ using RXN.AspNetCore.QuickBookWCSoap2Rest.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace RXN.AspNetCore.QuickBookWCSoap2Rest.Bridges
--- RXN.AspNetCore.QuickBookWCSoap2Rest/WCController.cs
+++ RXN.AspNetCore.QuickBookWCSoap2Rest/WCController.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Http;
 using RXN.AspNetCore.QuickBookWCSoap2Rest.Bridges;
 using RXN.AspNetCore.QuickBookWCSoap2Rest.Interfaces;
 using RXN.AspNetCore.QuickBookWCSoap2Rest.Utils;
-using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -40,45 +38,45 @@ namespace RXN.AspNetCore.QuickBookWCSoap2Rest
             var s2r = new WCRequestBridge(request);
             var soapAction = s2r.GetSkeletonActionMethod();
 
-            object responeValue;
+            object responseValue = null;
 
             switch (soapAction)
             {
                 //case WCSkeletonWebMethod.GetInteractiveURL:
-                //    responeValue = _hanlder.GetInteractiveURL(
+                //    responseValue = _hanlder.GetInteractiveURL(
                 //        s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET),
                 //        s2r.GetParam(WC_REQUEST_PARAMS.SESSION_ID)
                 //    );
                 //    break;
 
                 //case WCSkeletonWebMethod.InteractiveRejected:
-                //    responeValue = _hanlder.InteractiveRejected(
+                //    responseValue = _hanlder.InteractiveRejected(
                 //        s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET),
                 //        s2r.GetParam(WC_REQUEST_PARAMS.REASON)
                 //    );
                 //    break;
 
                 //case WCSkeletonWebMethod.InteractiveDone:
-                //    responeValue = _hanlder.InteractiveDone(s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET));
+                //    responseValue = _hanlder.InteractiveDone(s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET));
                 //    break;
 
                 case WCSkeletonWebMethod.ServerVersion:
-                    responeValue = _hanlder.ServerVersion(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
+                    responseValue = _hanlder.ServerVersion(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
                     break;
 
                 case WCSkeletonWebMethod.ClientVersion:
-                    responeValue = _hanlder.ClientVersion(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
+                    responseValue = _hanlder.ClientVersion(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
                     break;
 
                 case WCSkeletonWebMethod.Authenticate:
-                    responeValue = _hanlder.Authenticate(
+                    responseValue = _hanlder.Authenticate(
                         s2r.GetParam(WC_REQUEST_PARAMS.AUTH_USERNAME),
                         s2r.GetParam(WC_REQUEST_PARAMS.AUTH_PASSWORD)
                     );
                     break;
 
                 case WCSkeletonWebMethod.SendRequestXML:
-                    responeValue = _hanlder.SendRequestXML(
+                    responseValue = _hanlder.SendRequestXML(
                         s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
                         s2r.GetParam(WC_REQUEST_PARAMS.HCP_RESPONSE),
                         s2r.GetParam(WC_REQUEST_PARAMS.COMPANY_FILENAME),
@@ -89,7 +87,7 @@ namespace RXN.AspNetCore.QuickBookWCSoap2Rest
                     break;
 
                 case WCSkeletonWebMethod.ReceiveResponseXML:
-                    responeValue = _hanlder.ReceiveResponseXML(
+                    responseValue = _hanlder.ReceiveResponseXML(
                         s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
                         s2r.GetParam(WC_REQUEST_PARAMS.RESPONSE),
                         s2r.GetParam(WC_REQUEST_PARAMS.H_RESULT),
@@ -98,7 +96,7 @@ namespace RXN.AspNetCore.QuickBookWCSoap2Rest
                     break;
 
                 case WCSkeletonWebMethod.ConnectionError:
-                    responeValue = _hanlder.ConnectionError(
+                    responseValue = _hanlder.ConnectionError(
                         s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
                         s2r.GetParam(WC_REQUEST_PARAMS.H_RESULT),
                         s2r.GetParam(WC_REQUEST_PARAMS.MESSAGE)
@@ -106,163 +104,161 @@ namespace RXN.AspNetCore.QuickBookWCSoap2Rest
                     break;
 
                 case WCSkeletonWebMethod.GetLastError:
-                    responeValue = _hanlder.GetLastError(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
+                    responseValue = _hanlder.GetLastError(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
                     break;
 
                 case WCSkeletonWebMethod.CloseConnection:
-                    responeValue = _hanlder.CloseConnection(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
+                    responseValue = _hanlder.CloseConnection(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
                     break;
 
                 default:
-                    return null;
+                    break;
             }
 
-            var resBridge = new WCResponseBridge(soapAction, responeValue);
+            if (responseValue == null)
+            {
+                return null;
+            }
+
+            var resBridge = new WCResponseBridge(soapAction, responseValue);
 
             return resBridge.ResponseXml();
         }
 
         public async Task<XElement> HandleAsync(HttpRequest request)
         {
-            try
+            var s2r = new WCRequestBridge(request);
+            var soapAction = s2r.GetSkeletonActionMethod();
+
+            object responseValue = null;
+
+            switch (soapAction)
             {
-                var s2r = new WCRequestBridge(request);
-                var soapAction = s2r.GetSkeletonActionMethod();
+                //case WCSkeletonWebMethod.GetInteractiveURL:
+                //    var taskGetInteractiveURL = _hanlderAsync.GetInteractiveURLAsync(
+                //        s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET),
+                //        s2r.GetParam(WC_REQUEST_PARAMS.SESSION_ID)
+                //    );
+                //    if (taskGetInteractiveURL != null)
+                //    {
+                //        responseValue = await taskGetInteractiveURL;
+                //    }
+                //    break;
 
-                object responseValue = null;
+                //case WCSkeletonWebMethod.InteractiveRejected:
+                //    var taskInteractiveRejected = _hanlderAsync.InteractiveRejectedAsync(
+                //        s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET),
+                //        s2r.GetParam(WC_REQUEST_PARAMS.REASON)
+                //    );
+                //    if (taskInteractiveRejected != null)
+                //    {
+                //        responseValue = await taskInteractiveRejected;
+                //    }
+                //    break;
 
-                switch (soapAction)
-                {
-                    //case WCSkeletonWebMethod.GetInteractiveURL:
-                    //    var taskGetInteractiveURL = _hanlderAsync.GetInteractiveURLAsync(
-                    //        s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET),
-                    //        s2r.GetParam(WC_REQUEST_PARAMS.SESSION_ID)
-                    //    );
-                    //    if (taskGetInteractiveURL != null)
-                    //    {
-                    //        responseValue = await taskGetInteractiveURL;
-                    //    }
-                    //    break;
+                //case WCSkeletonWebMethod.InteractiveDone:
+                //    var taskInteractiveDone = _hanlderAsync.InteractiveDoneAsync(s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET));
+                //    if (taskInteractiveDone != null)
+                //    {
+                //        responseValue = await taskInteractiveDone;
+                //    }
+                //    break;
 
-                    //case WCSkeletonWebMethod.InteractiveRejected:
-                    //    var taskInteractiveRejected = _hanlderAsync.InteractiveRejectedAsync(
-                    //        s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET),
-                    //        s2r.GetParam(WC_REQUEST_PARAMS.REASON)
-                    //    );
-                    //    if (taskInteractiveRejected != null)
-                    //    {
-                    //        responseValue = await taskInteractiveRejected;
-                    //    }
-                    //    break;
+                case WCSkeletonWebMethod.ServerVersion:
+                    var taskServerVersion = _hanlderAsync.ServerVersionAsync(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
+                    if (taskServerVersion != null)
+                    {
+                        responseValue = await taskServerVersion;
+                    }
+                    break;
 
-                    //case WCSkeletonWebMethod.InteractiveDone:
-                    //    var taskInteractiveDone = _hanlderAsync.InteractiveDoneAsync(s2r.GetParam(WC_REQUEST_PARAMS.WC_TICKET));
-                    //    if (taskInteractiveDone != null)
-                    //    {
-                    //        responseValue = await taskInteractiveDone;
-                    //    }
-                    //    break;
+                case WCSkeletonWebMethod.ClientVersion:
+                    var taskClientVersion = _hanlderAsync.ClientVersionAsync(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
+                    if (taskClientVersion != null)
+                    {
+                        responseValue = await taskClientVersion;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.ServerVersion:
-                        var taskServerVersion = _hanlderAsync.ServerVersionAsync(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
-                        if (taskServerVersion != null)
-                        {
-                            responseValue = await taskServerVersion;
-                        }
-                        break;
+                case WCSkeletonWebMethod.Authenticate:
+                    var taskAuthenticate = _hanlderAsync.AuthenticateAsync(
+                        s2r.GetParam(WC_REQUEST_PARAMS.AUTH_USERNAME),
+                        s2r.GetParam(WC_REQUEST_PARAMS.AUTH_PASSWORD)
+                    );
+                    if (taskAuthenticate != null)
+                    {
+                        responseValue = await taskAuthenticate;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.ClientVersion:
-                        var taskClientVersion = _hanlderAsync.ClientVersionAsync(s2r.GetParam(WC_REQUEST_PARAMS.VERSION));
-                        if (taskClientVersion != null)
-                        {
-                            responseValue = await taskClientVersion;
-                        }
-                        break;
+                case WCSkeletonWebMethod.SendRequestXML:
+                    var task = _hanlderAsync.SendRequestXMLAsync(
+                        s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
+                        s2r.GetParam(WC_REQUEST_PARAMS.HCP_RESPONSE),
+                        s2r.GetParam(WC_REQUEST_PARAMS.COMPANY_FILENAME),
+                        s2r.GetParam(WC_REQUEST_PARAMS.XML_COUNTRY),
+                        int.Parse(s2r.GetParam(WC_REQUEST_PARAMS.XML_MAJOR_VERS)),
+                        int.Parse(s2r.GetParam(WC_REQUEST_PARAMS.XML_MINOR_VERS))
+                    );
+                    if (task != null)
+                    {
+                        responseValue = await task;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.Authenticate:
-                        var taskAuthenticate = _hanlderAsync.AuthenticateAsync(
-                            s2r.GetParam(WC_REQUEST_PARAMS.AUTH_USERNAME),
-                            s2r.GetParam(WC_REQUEST_PARAMS.AUTH_PASSWORD)
-                        );
-                        if (taskAuthenticate != null)
-                        {
-                            responseValue = await taskAuthenticate;
-                        }
-                        break;
+                case WCSkeletonWebMethod.ReceiveResponseXML:
+                    var taskReceiveResponseXML = _hanlderAsync.ReceiveResponseXMLAsync(
+                        s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
+                        s2r.GetParam(WC_REQUEST_PARAMS.RESPONSE),
+                        s2r.GetParam(WC_REQUEST_PARAMS.H_RESULT),
+                        s2r.GetParam(WC_REQUEST_PARAMS.MESSAGE)
+                    );
+                    if (taskReceiveResponseXML != null)
+                    {
+                        responseValue = await taskReceiveResponseXML;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.SendRequestXML:
-                        var task = _hanlderAsync.SendRequestXMLAsync(
-                            s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
-                            s2r.GetParam(WC_REQUEST_PARAMS.HCP_RESPONSE),
-                            s2r.GetParam(WC_REQUEST_PARAMS.COMPANY_FILENAME),
-                            s2r.GetParam(WC_REQUEST_PARAMS.XML_COUNTRY),
-                            int.Parse(s2r.GetParam(WC_REQUEST_PARAMS.XML_MAJOR_VERS)),
-                            int.Parse(s2r.GetParam(WC_REQUEST_PARAMS.XML_MINOR_VERS))
-                        );
-                        if (task != null)
-                        {
-                            responseValue = await task;
-                        }
-                        break;
+                case WCSkeletonWebMethod.ConnectionError:
+                    var taskConnectionError = _hanlderAsync.ConnectionErrorAsync(
+                        s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
+                        s2r.GetParam(WC_REQUEST_PARAMS.H_RESULT),
+                        s2r.GetParam(WC_REQUEST_PARAMS.MESSAGE)
+                    );
+                    if (taskConnectionError != null)
+                    {
+                        responseValue = await taskConnectionError;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.ReceiveResponseXML:
-                        var taskReceiveResponseXML = _hanlderAsync.ReceiveResponseXMLAsync(
-                            s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
-                            s2r.GetParam(WC_REQUEST_PARAMS.RESPONSE),
-                            s2r.GetParam(WC_REQUEST_PARAMS.H_RESULT),
-                            s2r.GetParam(WC_REQUEST_PARAMS.MESSAGE)
-                        );
-                        if (taskReceiveResponseXML != null)
-                        {
-                            responseValue = await taskReceiveResponseXML;
-                        }
-                        break;
+                case WCSkeletonWebMethod.GetLastError:
+                    var taskGetLastError = _hanlderAsync.GetLastErrorAsync(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
+                    if (taskGetLastError != null)
+                    {
+                        responseValue = await taskGetLastError;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.ConnectionError:
-                        var taskConnectionError = _hanlderAsync.ConnectionErrorAsync(
-                            s2r.GetParam(WC_REQUEST_PARAMS.TICKET),
-                            s2r.GetParam(WC_REQUEST_PARAMS.H_RESULT),
-                            s2r.GetParam(WC_REQUEST_PARAMS.MESSAGE)
-                        );
-                        if (taskConnectionError != null)
-                        {
-                            responseValue = await taskConnectionError;
-                        }
-                        break;
+                case WCSkeletonWebMethod.CloseConnection:
+                    var taskCloseConnection = _hanlderAsync.CloseConnectionAsync(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
+                    if (taskCloseConnection != null)
+                    {
+                        responseValue = await taskCloseConnection;
+                    }
+                    break;
 
-                    case WCSkeletonWebMethod.GetLastError:
-                        var taskGetLastError = _hanlderAsync.GetLastErrorAsync(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
-                        if (taskGetLastError != null)
-                        {
-                            responseValue = await taskGetLastError;
-                        }
-                        break;
-
-                    case WCSkeletonWebMethod.CloseConnection:
-                        var taskCloseConnection = _hanlderAsync.CloseConnectionAsync(s2r.GetParam(WC_REQUEST_PARAMS.TICKET));
-                        if (taskCloseConnection != null)
-                        {
-                            responseValue = await taskCloseConnection;
-                        }
-                        break;
-
-                    default:
-                        break;
-                }
-
-                if (responseValue == null)
-                {
-                    return null;
-                }
-
-                var resBridge = new WCResponseBridge(soapAction, responseValue);
-
-                return resBridge.ResponseXml();
+                default:
+                    break;
             }
-            catch (Exception ex)
+
+            if (responseValue == null)
             {
-                throw ex;
+                return null;
             }
+
+            var resBridge = new WCResponseBridge(soapAction, responseValue);
+
+            return resBridge.ResponseXml();
         }
     }
 }
